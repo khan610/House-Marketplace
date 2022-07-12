286fbed7714950302cc3659a76603d043cfdfbae
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-07-12 17:16:28 +0200

    Update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,54 @@
 import { useState, useEffect } from "react";
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile } from "firebase/auth";
+import { useNavigate, Link } from "react-router-dom";
+import { db } from "../firebase.config";
+import { updateDoc } from "firebase/firestore";
 
 function Profile() {
-  const [user, setUser] = useState(null);
-
   const auth = getAuth();
-  useEffect(() => {
-    setUser(auth.currentUser);
-  }, []);
+  const [changeDetails, setChangeDetails] = useState(false);
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
 
-  return user ? <h1>{user.displayName}</h1> : "Not Logged In";
+  const { name, email } = formData;
+
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
+  const onSubmit = () => {
+    console.log(123);
+  };
+
+  return (
+    <div className="profike">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={onLogout}></button>
+      </header>
+
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">
+            <p
+              className="changePersonalDetails"
+              onClick={() => {
+                changeDetails && onSubmit();
+                setChangeDetails((prevState) => !prevState);
+              }}
+            >
+              {changeDetails ? "done" : "change"}
+            </p>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Profile;
