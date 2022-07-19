55b1d75def8414f5be14fa3771a9dd39f14c0ddc
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-07-19 23:40:09 +0200

    Update

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
+
 function Category() {
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+
+  useEffect(() => {});
+
   return <div>Category</div>;
 }
 
